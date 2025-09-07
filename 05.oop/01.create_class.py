c9e185edebe0f766300c36295ceec31ef5fa8c78
MOHD ABUZAR ANSARI <164314033+mohdabuzaransari9@users.noreply.github.com>
2025-09-07 23:58:22 +0530

    Delete 05.oop directory

--- 05.oop/01.create_class.py
+++
@@ -1,6 +0,0 @@
-class StarCookie:
-    pass
- 
-
-star_cookie1=StarCookie()
-print(star_cookie1)
--- 05.oop/02.create_attribute.py
+++
@@ -1,7 +0,0 @@
-class StarCookie:
-    def __init__(self,color):
-        self.color=color
-
-
-star_cookie1= StarCookie("red")
-print(star_cookie1.color)
