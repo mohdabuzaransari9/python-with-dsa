3ea2214524e51141dc011e007203c5d1e8d7b5e8
MOHD ABUZAR ANSARI <164314033+mohdabuzaransari9@users.noreply.github.com>
2025-09-07 23:56:30 +0530

    Add files via upload

---
+++ DSA/05.oop/01.create_class.py
@@ -0,0 +1,6 @@
+class StarCookie:
+    pass
+ 
+
+star_cookie1=StarCookie()
+print(star_cookie1)
---
+++ DSA/05.oop/02.create_attribute.py
@@ -0,0 +1,7 @@
+class StarCookie:
+    def __init__(self,color):
+        self.color=color
+
+
+star_cookie1= StarCookie("red")
+print(star_cookie1.color)
