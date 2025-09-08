0ad8f20be67ed72531254e10dee930849a73fd66
MOHD ABUZAR ANSARI <164314033+mohdabuzaransari9@users.noreply.github.com>
2025-09-09 02:27:06 +0530

    Add files via upload

---
+++ DSA/06.linked list/08.linked_list_insert_append_prepend_node_with_print_by___str__.py
@@ -0,0 +1,98 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+
+
+
+class LinkedList:
+
+    def __init__(self):
+        
+        self.head=None
+        self.tail=None
+        self.length=0
+
+    def __str__(self):
+        temp_node=self.head
+        result= ''
+        while temp_node is not None:
+            result +=str(temp_node.value)
+            if temp_node.next is not None:
+                result +=' -> '
+            temp_node=temp_node.next
+    
+        return  result
+    
+
+    def append(self,value):
+
+        new_node=Node(value)
+        if self.head is None:
+
+            self.head=new_node
+            self.tail=new_node
+
+        else:
+            self.tail.next=new_node
+            self.tail= new_node
+
+            
+        self.length+=1
+
+    def prepend(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+        self.length+=1
+
+    def insert(self,index,value):
+        new_node=Node(value)
+
+        if index<0 or index>self.length:         #  negative edge case orout of range edge case
+            return False
+        elif self.length==0:                     # empty linkedlist
+            self.head=new_node
+            self.tail=new_node
+
+        elif index==0:                          # 0th index linkedlist
+            new_node.next=self.head
+            self.head=new_node
+
+        else:                                   # else
+            temp_node=self.head
+            for _ in range(index-1):
+                temp_node=temp_node.next
+            new_node.next=temp_node.next
+            temp_node.next=new_node
+
+            
+
+        self.length+=1
+        return True
+
+
+
+
+
+
+new_linked_List=LinkedList() 
+new_linked_List.prepend(10)   
+new_linked_List.append(20)
+print(new_linked_List)
+new_linked_List.prepend(5)   
+print(new_linked_List)
+new_linked_List.insert(2,100)   # insert middle
+print(new_linked_List)
+new_linked_List.insert(0,1)   # insert beginning
+print(new_linked_List)
+
+new_linked_List.insert(100,1000)   # insert end edge case out of range
+
+if new_linked_List!=new_linked_List:
+    print(new_linked_List)
---
+++ DSA/06.linked list/11.set_value_by_index.py
@@ -0,0 +1,143 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+
+
+
+class LinkedList:
+
+    def __init__(self):
+        
+        self.head=None
+        self.tail=None
+        self.length=0
+
+    def __str__(self):
+        temp_node=self.head
+        result= ''
+        while temp_node is not None:
+            result +=str(temp_node.value)
+            if temp_node.next is not None:
+                result +=' -> '
+            temp_node=temp_node.next
+    
+        return  result
+    
+
+    def append(self,value):
+
+        new_node=Node(value)
+        if self.head is None:
+
+            self.head=new_node
+            self.tail=new_node
+
+        else:
+            self.tail.next=new_node
+            self.tail= new_node
+
+            
+        self.length+=1
+
+    def prepend(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+        self.length+=1
+
+    def insert(self,index,value):
+        new_node=Node(value)
+
+        if index<0 or index>self.length:         #  negative edge case orout of range edge case
+            return False
+        elif self.length==0:                     # empty linkedlist
+            self.head=new_node
+            self.tail=new_node
+
+        elif index==0:                          # 0th index linkedlist
+            new_node.next=self.head
+            self.head=new_node
+
+        else:                                   # else
+            temp_node=self.head
+            for _ in range(index-1):
+                temp_node=temp_node.next
+            new_node.next=temp_node.next
+            temp_node.next=new_node
+
+            
+
+        self.length+=1
+        return True
+    
+    def traverse(self):
+        current=self.head
+        while current:  # while current is not None
+            print(current.value)
+            current=current.next
+
+    def search(self,target):
+        current=self.head
+        index=0
+        while current:
+            if current.value==target:
+                print(current.value)
+                return index
+            current=current.next
+            index+=1
+        return -1
+
+    def get(self, index):
+
+        if index==-1:
+            return self.tail
+        if index<-1 or index >= self.length:
+            return None
+        current=self.head
+        for _ in range(index):
+            current=current.next
+        return current
+    
+    def set(self,index,value):
+        temp=self.get(index)
+        if temp:
+            temp.value=value
+            return True
+        return False
+    
+
+
+new_linked_List=LinkedList() 
+new_linked_List.prepend(10)   
+new_linked_List.append(20)
+print(new_linked_List)
+new_linked_List.prepend(5)   
+print(new_linked_List)
+new_linked_List.insert(2,100)   # insert middle
+print(new_linked_List)
+new_linked_List.insert(0,1)   # insert beginning
+print(new_linked_List)
+
+new_linked_List.insert(100,1000)   # insert end edge case out of range
+
+if new_linked_List!=new_linked_List:
+    print(new_linked_List)
+
+
+new_linked_List.traverse()
+print(new_linked_List.search(140))
+
+
+print(new_linked_List.search(140))
+
+print(new_linked_List)
+print(new_linked_List.get(2))
+
+print(new_linked_List)
+print(new_linked_List.set(6,200))
