e6b0bc73915fa17ce06750596eedb1b2204695a9
MOHD ABUZAR ANSARI <164314033+mohdabuzaransari9@users.noreply.github.com>
2025-10-04 23:53:18 +0530

    Add files via upload

---
+++ DSA/09.circular double linked list/03.append.py
@@ -0,0 +1,54 @@
+class Node:
+
+    def __init__(self,value):
+        self.value=value
+        self.prev=None
+        self.next=None
+
+    def __str__(self):
+        return str(self.value)
+
+class CircularDoublyLinkedList:
+
+    def __init__(self, value=None):
+        if value is None:       # if value is not given create empty cdlinkedlist
+            self.head = None
+            self.tail = None
+            self.length = 0
+        else:                   # if value is given create cdlinkedlist with one node
+            new_node = Node(value)
+            new_node.next = new_node
+            new_node.prev = new_node
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+
+
+    def append(self,value):
+        new_node=Node(value)
+        if self.length==0:
+            self.head=new_node
+            self.tail=new_node
+            new_node.next=new_node
+            new_node.prev=new_node
+        else:
+            self.tail.next=new_node
+            self.head.prev=new_node
+            new_node.prev=self.tail
+            new_node.next=self.head
+            self.tail=new_node
+
+        self.length+=1
+        
+
+
+
+
+cdll=CircularDoublyLinkedList()
+
+print("append")
+cdll.append(10)
+cdll.append(20)
+print(cdll.head)
+
+
